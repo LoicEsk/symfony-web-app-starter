b328a3ef97403c7732cfd8270060f8f57b468e45
Loïc Laurent <llaurent@selexium.com>
2023-10-20 08:18:16 +0200

    login form

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -5,7 +5,7 @@
 docker compose run --rm sf composer Install
 
 # Liste des migrations Doctrine
-docker compose rm --rm sf php bin/console doctrine:migrations:list
+docker compose run --rm sf php bin/console doctrine:migrations:list
 
 # Install des packet JS
 docker compose run --rm encore npm ci
