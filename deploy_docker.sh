1112ea7cefd04d2fb54f5ea380fd95d542db571e
Loïc <loic.laurent@outlook.com>
2023-10-29 21:20:58 +0100

    remove secret

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -4,9 +4,10 @@
 #  Install des packet php
 docker compose run --rm sf composer Install
 
-# Liste des migrations Doctrine
-docker compose run --rm sf php bin/console doctrine:migrations:list
 
 # Install des packet JS
 docker compose run --rm encore npm ci
 docker compose run --rm encore npm run dev
+
+# Liste des migrations Doctrine
+docker compose run --rm sf php bin/console doctrine:migrations:list
--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class WelcomeController extends AbstractController
 {
     #[Route('/welcome', name: 'app_welcome')]
     public function index(): Response
