d01db23315406e583c6a7da28627abc009968f9f
Loïc Laurent <llaurent@selexium.com>
2024-04-03 12:13:44 +0200

    force des reset de bdd test

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -16,7 +16,7 @@ if [ $exit_code -ne 0 ]; then
 # else
 #     echo "Base de données de tests à jour"
 fi
-docker compose run --rm sf php bin/console doctrine:fixtures:load --env=test
+docker compose run --rm sf php bin/console doctrine:fixtures:load --env=test --no-interaction
 
 # docker compose run --rm sf php bin/console doctrine:migrations:migrate --env=test -y
 
