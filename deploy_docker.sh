c4c44b32f18fa729d31585e217392099ed66642b
Loïc Laurent <llaurent@selexium.com>
2024-04-03 11:39:54 +0200

    chargement des fixtures de tests à chaque deploy

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -13,10 +13,10 @@ exit_code=$?
 if [ $exit_code -ne 0 ]; then
     docker compose run --rm sf php bin/console doctrine:database:create --env=test
     docker compose run --rm sf php bin/console doctrine:migrations:migrate --env=test --no-interaction
-    docker compose run --rm sf php bin/console doctrine:fixtures:load --env=test
 # else
 #     echo "Base de données de tests à jour"
 fi
+docker compose run --rm sf php bin/console doctrine:fixtures:load --env=test
 
 # docker compose run --rm sf php bin/console doctrine:migrations:migrate --env=test -y
 
