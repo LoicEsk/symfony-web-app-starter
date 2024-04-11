24a6bb754bee69f90d9a5d2e72d51a808a5cea0f
Loïc <loic.laurent@outlook.com>
2024-04-11 11:24:26 +0200

    test accès BO

---
+++ tests/Controller/AdminControllerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Entity\User;
+
+class AdminControllerTest extends WebTestCase
+{
+    public function testIndex(): void
+    {
+        $client = static::createClient();
+        
+        // Accès non loggé interdit
+        $crawler = $client->request('GET', '/admin');
+        $this->assertResponseStatusCodeSame(302); // redir vers le login attendu
+
+        $user = $this->getAdminUser();
+        $client->loginUser( $user );
+
+        $crawler = $client->request('GET', '/admin');
+        self::assertResponseStatusCodeSame(200);
+
+    }
+
+    /**
+     * Méthodes privées
+     */
+
+     private function getAdminUser(): User
+     {
+         // Récupère le gestionnaire d'entités
+         $entityManager = static::$kernel->getContainer()->get('doctrine')->getManager();
+     
+         // Recherche l'utilisateur par son email
+         $userRepository = $entityManager->getRepository(User::class);
+         $user = $userRepository->findOneBy(['email' => 'admin@local.app' ]); // le dernier utlisateur
+     
+         $this->assertNotNull( $user, 'Aucun utilsateur trouvé' );
+         return $user;
+     }
+}
