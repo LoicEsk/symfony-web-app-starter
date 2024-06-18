cbe1a1c069ef6aee4a79d0747626b1a7210edca6
Loïc Laurent <llaurent@selexium.com>
2024-06-18 12:15:36 +0200

    fix compile assets

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ php bin/console doctrine:migrations:list
 php bin/console doctrine:migrations:migrate
 
 # compilation des assets
-php bin/console sass:build
 php bin/console importmap:install
+php bin/console sass:build
 php bin/console asset-map:compile
